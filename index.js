d3375d2dbf9b4cd3b39e2110b27d1f1e44ea69d2
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-06-22 15:31:36 -0400

    add endpoints to documentation and index.js

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 const express = require('express'),
   morgan = require('morgan');
-
+  bodyParser = require('body-parser');
+const { parse } = require('uuid');
+        
 const app = express();
+  
+app.use(bodyParser.json());
+  
+//Get documentation
+app.use(express.static('public'));
+app.get('/documentation.html', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+let ourMovies = [
+    {
+        title: 'Harry Potter',
+        description: 'Harry Potter is a film series based on the eponymous novels by J. K. Rowling. The series is distributed by Warner Bros. and consists of eight fantasy films',
+        genre: {
+            name: 'Fantasy',
+            description: 'Fantasy films are films with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds.'
+        },
+        director: 'Chris Columbus, David Yates'
+    },
+    {
+        title: 'Avengers',
+		description:'The director of the agency S.H.I.E.L.D., Nick Fury, sets in motion project Avengers, joining Tony Stark a.k.a. the Iron Man; Steve Rogers, a.k.a. Captain America; Bruce Banner, a.k.a. The Hulk; Thor; Natasha Romanoff, a.k.a. Black Widow; and Clint Barton, a.k.a. Hawkeye, to save the world from the powerful Loki and the alien invasion.',
+		genre: {
+            name: 'Superhero',
+            description: 'Films that focuses on the actions of superheroes: individuals who usually possess extraordinary – generally superhuman – abilities and are dedicated to protecting the public.'
+        },
+        director: '',    
+    }
+]
+
+//Starting request
+app.get('/', (req, res) => {
+	res.send('Welcome to myFlix!');
+});
+
+//ENDPOINT DOC START HERE
+//Allow new users to register
+app.post('/users', (req, res) => {
+    res.send('Successful registration of new user!')
+});
+
+
+
+//Return a list of all movies
+app.get('/movies', (req, res) => {
+    res.json(ourMovies);
+});
+
+
 
 // logging
 app.use(morgan('common'));
@@ -11,37 +62,10 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.use(express.static('public'));
-
-
-// GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to myFlix!');
-});
-
-app.get('/movies', (req, res) => {
-    res.json([
-        {movie1: 'top1'},
-        {movie2: 'top2'},
-        {movie3: 'top3'},
-        {movie4: 'top4'},
-        {movie5: 'top5'},
-        {movie6: 'top6'},
-        {movie7: 'top7'},
-        {movie8: 'top8'},
-        {movie9: 'top9'},
-        {movie10: 'top10'}
-    ]);
-});
-
-app.get('/documentation.html', (req, res) => {
-    res.sendFile('public/documentation.html', { root: __dirname });
-});
-  
-
 // listen for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
+
 
 
