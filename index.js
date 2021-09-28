4c97829d8c96d1b4ee1fbe8f7c18d46b06df77f4
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-09-28 13:47:58 -0400

    Problem-solve CORS error

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let allowedOrigins = [
   "https://myflix-application-client.netlify.app/"
 ];
 
-app.use(cors({
+/* app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) { //If a specific origin isn’t found on the list
@@ -42,7 +42,7 @@ app.use(cors({
     }
     return callback(null, true);
   }
-}));
+})); */
 
 //Get documentation
 
